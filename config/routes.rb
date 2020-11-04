d4322a85c2a2ffd31edd0c9d1351b4983de5ab85
anil ekici <anilcanekici@gmail.com>
2020-11-04 14:46:17 +0000

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'restaurants#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,22 @@ puts "DESTROYING RESTAURANTS"
 Restaurant.destroy_all
 
 20.times do 
-    name = Faker::Restaurant.name
+  name = Faker::Restaurant.name
 
-    restaurant = Restaurant.create!(
-        name: name,
-        address: Faker::Address.full_address,
-        phone_number: Faker::PhoneNumber.cell_phone_in_e164,
-        category: %w( chinese italian japanese french belgian ).sample )
+  restaurant = Restaurant.create!(
+    name: name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: %w( chinese italian japanese french belgian ).sample
+  )
     
-    puts "CREATED #{name}"
-    puts "WRITING REVIEW"
-    4.times do
-      Review.create!(
-        rating: rand(0..5),
-        content: Faker::Games::Witcher.quote,
-        restaurant_id: restaurant.id
-      )
-    end
+  puts "CREATED #{name}"
+  puts "WRITING REVIEW"
+  4.times do
+  Review.create!(
+    rating: rand(0..5),
+    content: Faker::Games::Witcher.quote,
+    restaurant_id: restaurant.id
+  )
+  end
 end
