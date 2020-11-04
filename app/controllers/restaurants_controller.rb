2c06ada90d387381b76bd857617ce4812c41c670
anil ekici <anilcanekici@gmail.com>
2020-11-04 16:33:46 +0000

    yelp mvp done without style

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,6 +13,11 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+        redirect_to restaurant_path(@restaurant.id), notice: "restaurant added"
+    else
+        render :new
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new', to: 'restaurants#new'
-  get 'restaurants/:id', to: 'restaurants#show'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  resources :restaurants, only: [ :index, :new, :create, :show] do
+    resources :reviews, only: [ :new, :create ]
+  end
+  # get 'restaurants/:id/reviews' to: 'restaurants#show'
 end
